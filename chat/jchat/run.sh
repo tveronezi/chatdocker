44aa8f18375ec2c2d009000ed7c6ac895547218f
Thiago Veronezi <thiago@veronezi.org>
2014-08-18 02:48:21 +0000

    making 8080 and 8081 public

--- chat/jchat/run.sh
+++ chat/jchat/run.sh
@@ -2,4 +2,4 @@
 docker kill jchat || true
 docker rm jchat || true
 docker build -t jchat .
-docker run -d --name=jchat jchat
+docker run -d -p 8080:8080 --name=jchat jchat
--- chat/springchat/run.sh
+++ chat/springchat/run.sh
@@ -2,4 +2,4 @@
 docker kill springchat || true
 docker rm springchat || true
 docker build -t springchat .
-docker run -d --link=jchat:jchat --name=springchat springchat
+docker run -d  -p 8081:8080 --link=jchat:jchat --name=springchat springchat
